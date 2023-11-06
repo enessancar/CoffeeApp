9837dab7837115d3b0e2315f6e99c4ab145500a7
enessancar <enes57751907@gmail.com>
2023-11-06 13:14:22 +0300

    CoffeDetail vc and view added User manager added register, search , discover screen created

---
+++ CoffeeApp/Feature/Detail/CoffeeDetailVC.swift
@@ -0,0 +1,8 @@
+//
+//  CoffeeDetailVC.swift
+//  CoffeeApp
+//
+//  Created by Enes Sancar on 5.11.2023.
+//
+
+import Foundation
---
+++ CoffeeApp/Feature/Detail/CoffeeDetailView.swift
@@ -0,0 +1,8 @@
+//
+//  CoffeDetailView.swift
+//  CoffeeApp
+//
+//  Created by Enes Sancar on 5.11.2023.
+//
+
+import Foundation
